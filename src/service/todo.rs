6c57b0be707f79aa03f75d701434cb1a23a7c32b
kimjooyoon <asketeddy@gmail.com>
2023-10-09 21:18:10 +0900

    test(todo): add service retrieve_todo_by_id fn

--- src/service/todo.rs
+++ src/service/todo.rs
@@ -28,6 +28,35 @@ impl TodoService for TodoServiceAdapter<'_> {
     }
 
     fn save(&mut self, todo: Todo) -> i64 {
-        self.repository.save(todo, || {false})
+        self.repository.save(todo, || { false })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use mockall::predicate::eq;
+    use crate::domain::entities::Todo;
+    use crate::domain::repositories::{MockTodoRepository, TodoRepository};
+    use crate::service::todo::{TodoService, TodoServiceAdapter};
+
+    #[test]
+    fn retrieve_todo_by_id<'a>() {
+        let mut mock = MockTodoRepository::new();
+        mock.expect_by_id()
+            .with(eq(1i64))
+            .returning(|_| Ok(Todo {
+                id: 1i64,
+                title: "이슈 만들기 title".to_string(),
+                content: "이슈 만들기 content".to_string(),
+                order_number: 1i32,
+            })).once();
+        let repository: Box<dyn TodoRepository + 'a> = Box::new(mock);
+
+        let result = TodoServiceAdapter::new(repository).retrieve_todo_by_id(1i64).expect("fail test");
+
+        assert_eq!(1i64, result.id);
+        assert_eq!("이슈 만들기 title".to_string(), result.title);
+        assert_eq!("이슈 만들기 content".to_string(), result.content);
+        assert_eq!(1i32, result.order_number);
     }
 }
